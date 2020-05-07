b81a20dc4315e0d62a644d412e47e6b1e289e845
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-05-07 12:23:02 +0200

    Create exept_def.py

---
+++ exept_def.py
@@ -0,0 +1,5 @@
+def exeption(a, b, c):
+    if a == 0:
+        print("\n\tIt doesn't exist quadratic equation with a = 0..")
+    elif b == 0 and c == 0:
+        print('\n\tThe solution of equation is:\n\n\t\tx = 0')
